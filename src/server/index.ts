7d5923c266ca7dbf27b96a6a671fe234761b5892
Camilla <logikasciuro@gmail.com>
2023-08-02 19:40:51 +0000

    📝 Swagger-UI

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,6 +5,14 @@ import { router } from '../routers';
 import { handleError } from '../middlewares/error-handlers/handlerError';
 import { handleTypeORMError } from '../middlewares/error-handlers/handleTypeORMError';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const swaggerUi = require('swagger-ui-express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const YAML = require('yamljs');
+
+
+const swaggerDocument = YAML.load('src/openapi/openapi.yml');
+
 dotenv.config();
 export const app = express();
 
@@ -13,6 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(handleTypeORMError);
 app.use(handleError);
